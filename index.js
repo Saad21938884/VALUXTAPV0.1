e0230d9aa26c5203bd3e200ee19f4ada76d8716b
Saad21938884 <sofiantahiri821@gmail.com>
2026-08-06 20:24:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,34 @@
+const TelegramBot = require("node-telegram-bot-api");
+
+const bot = new TelegramBot(process.env.BOT_TOKEN, {
+    polling: true
+});
+
+
+bot.onText(/\/start/, (msg) => {
+
+    const chatId = msg.chat.id;
+
+    bot.sendMessage(
+        chatId,
+        "🎮 أهلاً بك في Play Point\n\nاضغط على الزر للدخول إلى التطبيق 🚀",
+        {
+            reply_markup: {
+                inline_keyboard: [
+                    [
+                        {
+                            text: "🚀 فتح التطبيق",
+                            web_app: {
+                                url: "ضع رابط الـ Mini App هنا"
+                            }
+                        }
+                    ]
+                ]
+            }
+        }
+    );
+
+});
+
+
+console.log("Play Point Bot Started");
